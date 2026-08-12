dd946373d3e0a59e9a2afc278766ee4b16541700
ARPITHAD26 <28arpithad@gmail.com>
2026-08-12 20:27:23 +0530

    Update add-client.component.ts

--- HRMS/spoorthy-ui/src/app/clients/add-client/add-client.component.ts
+++ HRMS/spoorthy-ui/src/app/clients/add-client/add-client.component.ts
@@ -103,7 +103,7 @@ export class AddClientComponent implements OnInit {
     this.clientForm = new FormGroup({
       'name': new FormControl(null, [Validators.required, Validators.pattern('^[a-zA-Z- ]*$'), Validators.maxLength(100)]),
       'officePhoneNo': new FormControl(null, [Validators.pattern("^[+0-9\\s\\-()]*$"), Validators.maxLength(20)]),
-      'cantactNo': new FormControl(null, [Validators.required, Validators.pattern("^[+0-9\\s\\-()]*$"), Validators.maxLength(20)]),
+      'ContactNo': new FormControl(null, [Validators.required, Validators.pattern("^[+0-9\\s\\-()]*$"), Validators.maxLength(20)]),
       'email': new FormControl(null, [Validators.required, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
 
       'address': new FormControl(null, [Validators.required, Validators.maxLength(250)]),
